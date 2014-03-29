dcf680cfaaf64bdc924da6ff7106c6d22417d2e9
Matt Ewing <mewing6732@gmail.com>
2014-03-29 15:26:23 -0400

    First version of Flask web app. Addresses issue  #13

---
+++ pubmark-web.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__, static_folder='web/static', static_url_path='')
+app.template_folder = "web"
+
+@app.route("/")
+def landing():
+    return render_template('landing.html')
+    
+@app.route("/admin")
+def admin():
+    return render_template('admin.html')
+
+if __name__ == "__main__":
+    app.run()
