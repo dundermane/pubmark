2525494f18849712fa710ea995b46778bfaf6730
thequbit <thequbit@gmail.com>
2014-04-01 16:31:49 -0400

    updated tweet consumer to be a class, and removed creds from being commited to file

--- tweetconsumer.py
+++ tweetconsumer.py
@@ -1,50 +1,83 @@
 import twitter
 from dblayer import DBlayer
 
-pmdb = DBlayer()
-api = twitter.Api(consumer_key='GbQ2flsAujOSxEkGTee1Yw',
-	              consumer_secret='dH3lAHODXrot6yZ5T4TrKcMLukKRAUK4sQtaPIflUU',
-	              access_token_key='275002578-orQLeBDCwh8ZM70LL2kyyBGgauOKIEPlHzWKURmO',
-	              access_token_secret='mBnDZ7DenTOtZULZHiJkGRAviV14XHYVs0yLN1Hj3pVir',
-	              cache=None)
+class TweetConsumer(object):
+
+    def __init__(self,consumer_key,consumer_secret,access_token_key,access_token_secret,DEBUG):
+
+        self.DEBUG = DEBUG
+
+        self.pmdb = DBlayer()
+        self.api = twitter.Api(consumer_key=consumer_key,
+	                       consumer_secret=consumer_secret,
+	                       access_token_key=access_token_key,
+	                       access_token_secret=access_token_secret,
+	                       cache=None,
+        )
 	              
-merchantlist = pmdb.getmerchants()
-s = []
+        #self.merchantlist = self.pmdb.getmerchants()
+        #self.s = []
 
-def consume():
-    for merchant in merchantlist:
-	    if merchant['twitterid']==None:
-	        pulltwitterid(merchant)
-	    statuses = api.GetUserTimeline(user_id=merchant['twitterid'],
-	                                   include_rts=False,
-	                                   since_id=merchant['lasttweet'])
-	    for stat in reversed(statuses):
-	        pmdb.addtweet(filtertweet(stat))
-	        new = {'lastupdated':stat.created_at,
-	               'lasttweet':stat.id,
-	               'twitterhandle':stat.user.screen_name}
-	        pmdb.updatemerchant(merchant['_id'], new)
+        if self.DEBUG:
+            print "TweetConsumer() init successful."
+
+    def consume(self):
+
+        if self.DEBUG:
+            print "Reading merchant list from database ..."
+
+        # get the latest merchant list
+        merchantlist = self.pmdb.getmerchants()
+
+        statuses = []
+        for merchant in merchantlist:
+            if merchant['twitterid']==None:
+                self.pulltwitterid(merchant)
+                statuses.append(self.api.GetUserTimeline(user_id=merchant['twitterid'],
+                                include_rts=False,
+                                since_id=merchant['lasttweet'],
+                ))
+
+        if self.DEBUG:
+            print "Adding statuses to database ..."
+
+        for stat in reversed(statuses):
+            self.pmdb.addtweet(filtertweet(stat))
+            new = {'lastupdated':stat.created_at,
+                   'lasttweet':stat.id,
+                   'twitterhandle':stat.user.screen_name
+            }
+            self.pmdb.updatemerchant(merchant['_id'], new)
 	        
-	        print 'nom.'
+            print 'nom.'
 	        
 
-def filtertweet(s):
-    ftweet = {'id':str(s.id),
-              'merchant':str(s.user.id),
-              'merchanthandle':str(s.user.screen_name),
-              'created':str(s.created_at),
-              'geo':str(s.geo),
-              'hashtags':str(s.hashtags)}
-    return ftweet
+    def filtertweet(self,s):
+        ftweet = {'id':str(s.id),
+                  'merchant':str(s.user.id),
+                  'merchanthandle':str(s.user.screen_name),
+                  'created':str(s.created_at),
+                  'geo':str(s.geo),
+                  'hashtags':str(s.hashtags)}
+        return ftweet
 
-def pulltwitterid(merchant):
-    print "whoops, no twitterid, must be a new user"
-    user = api.GetUser(screen_name=merchant['twitterhandle'])
-    merchant['twitterid'] = user.id
-    print "found twitterid"
-    pmdb.updatemerchant(merchant['_id'],{'twitterid':merchant['twitterid']})
-    print "updated twitterid to db"
+    def pulltwitterid(self,merchant):
+        print "whoops, no twitterid, must be a new user"
+        user = self.api.GetUser(screen_name=merchant['twitterhandle'])
+        merchant['twitterid'] = user.id
+        print "found twitterid"
+        self.pmdb.updatemerchant(merchant['_id'],{'twitterid':merchant['twitterid']})
+        print "updated twitterid to db"
 
-		   
 if __name__=="__main__":
-    consume()
+
+    from twitter_creds import twitter_creds 
+
+    tc = TweetConsumer(consumer_key=twitter_creds['consumer_key'],
+                       consumer_secret=twitter_creds['consumer_secret'],
+                       access_token_key=twitter_creds['access_token_key'],
+                       access_token_secret=twitter_creds['access_token_secret'],
+                       DEBUG=True,
+    )
+
+    tc.consume()
---
+++ twitter_creds.py
@@ -0,0 +1,10 @@
+
+# fill this dictionary in with the correct values from twitter
+
+twitter_creds = {
+    'consumer_key':'',
+    'consumer_secret':'',
+    'access_token_key':'',
+    'access_token_secret':'',
+}
+
